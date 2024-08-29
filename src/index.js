48b8cdfe2e4f1d2c51ee18b79d050b9bb4875778
Niroopa R P <niroopalraju@gmail.com>
2024-08-29 18:08:45 +0530

    sd

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function StudentManagement() {
+  const [regular, setRegular] = useState(8);
+  const [remedial, setRemedial] = useState(13);
+  const [others, setOthers] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div class="main-content">
+      <div class="first-division">
+        <div class="management-content">
+          <h1>Logo</h1>
+        </div>
+        <div class="management-content">
+          <h1>Student Management</h1>
+        </div>
+        <div class="management-content">
+          <h1>Finanacial Management</h1>
+        </div>
+        <div class="management-content">
+          <h1>Quality control</h1>
+        </div>
+        <div class="management-content">
+          <h1>Report Delivery</h1>
+        </div>
+        <div class="management-content">
+          <h1>Attendence</h1>
+        </div>
+        <div class="management-content">
+          <h1>last</h1>
+        </div>
+      </div>
+      <div class="second-division">
+        <h1 class="topic">Student Management</h1>
+        <div class="count-content">
+          <div class="regular-content">
+            <h2>{regular}</h2>
+            <h1>Regular students</h1>
+          </div>
+          <div class="remedial-content">
+            <h2>{remedial}</h2>
+            <h1>Remidial Students</h1>
+          </div>
+          <div class="other-content">
+          <h2>{others}</h2>
+          <h1>In Food Club</h1>
+          </div>
+        </div>
+        <h3 class="menu-topic">Menu</h3>
+        <div class="menu-content">
+          <div class="menu-content-profile">
+            <h1>Regular Enrollment</h1>
+          </div>
+          <div class="menu-content-profile">
+            <h1>Remedial Enrollment</h1>
+          </div>
+          <div class="menu-content-profile">
+            <h1>Club Management</h1>
+          </div>
+          <div class="menu-content-profile">
+            <h1>Classroom Management</h1>
+          </div>
+          <div class="menu-content-profile">
+            <h1>SMS/EMAIL</h1>
+          </div>
+          <div class="menu-content-profile">
+            <h1>Attendence</h1>
+          </div>
+          <div class="menu-content-profile">
+            <h1>Clinic</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<StudentManagement />, document.getElementById('root'));
+ 
